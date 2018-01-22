ffc98db0884c0d3b243726aa0250d35cf13803c2
sdromacque <sebastien@citymagine.com>
2018-01-22 15:20:15 +0100

    Add boom, remove coveralls, update command yarn

--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -5,11 +5,6 @@ const authRoutes = require('./auth.route');
 const router = express.Router();
 
 /**
- * GET v1/status
- */
-router.get('/status', (req, res) => res.send('OK'));
-
-/**
  * GET v1/docs
  */
 router.use('/docs', express.static('docs'));
